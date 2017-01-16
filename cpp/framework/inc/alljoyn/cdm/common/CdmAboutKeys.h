b9dd72cf9477e4d7b7397984201994508ba775c0
Nathaniel Garbutt <nathaniel@two-bulls.com>
2017-01-16 21:35:45 +1100

    ASABASE-1115 replace copyright notices
    Change-Id: Ibfcfaca28b41e7ca4d2b7f908815fe3ab18e39f9
    Signed-off-by: Nathaniel Garbutt <nathaniel@two-bulls.com>

--- cpp/framework/inc/alljoyn/cdm/common/CdmAboutKeys.h
+++ cpp/framework/inc/alljoyn/cdm/common/CdmAboutKeys.h
@@ -1,17 +1,31 @@
 /******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
- *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
- *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * Copyright (c) 2016 Open Connectivity Foundation (OCF) and AllJoyn Open
+ *    Source Project (AJOSP) Contributors and others.
+ * 
+ *    SPDX-License-Identifier: Apache-2.0
+ * 
+ *    All rights reserved. This program and the accompanying materials are
+ *    made available under the terms of the Apache License, Version 2.0
+ *    which accompanies this distribution, and is available at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Copyright 2016 Open Connectivity Foundation and Contributors to
+ *    AllSeen Alliance. All rights reserved.
+ * 
+ *    Permission to use, copy, modify, and/or distribute this software for
+ *    any purpose with or without fee is hereby granted, provided that the
+ *    above copyright notice and this permission notice appear in all
+ *    copies.
+ * 
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ *     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ *     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ *     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ *     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *     PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
 #ifndef CDMABOUTKEYS_H_
 #define CDMABOUTKEYS_H_
@@ -30,16 +44,14 @@ namespace services {
          * @name Custome Cdm AboutFields
          *
          * The additional fields in the Cdm About interface
-         * TODO put in a table listing the properties for all of the about fields 
-         *      when similar todo in <alljoyn/AboutKeys.h> is implemented
          */
         // @{
-        
-static const qcc::String COUNTRY_OF_PRODUCTION;
-static const qcc::String CORPORATE_BRAND;
-static const qcc::String PRODUCT_BRAND;
-static const qcc::String LOCATION;
-static const qcc::String DEVICE_TYPE_DESCRIPTION;
+        static const qcc::String COUNTRY_OF_PRODUCTION; ///< The country of production
+        static const qcc::String CORPORATE_BRAND; ///< The brand name for the corporation
+        static const qcc::String PRODUCT_BRAND; ///< The brand name for the product
+        static const qcc::String LOCATION; ///< Where the device is located
+        static const qcc::String DEVICE_TYPE_DESCRIPTION; ///< A device type code and bus path
+        // @}
     };
 
 }
--- cpp/framework/inc/alljoyn/cdm/controller/CdmController.h
+++ cpp/framework/inc/alljoyn/cdm/controller/CdmController.h
@@ -1,17 +1,31 @@
 /******************************************************************************
- * Copyright AllSeen Alliance. All rights reserved.
- *
- *    Permission to use, copy, modify, and/or distribute this software for any
- *    purpose with or without fee is hereby granted, provided that the above
- *    copyright notice and this permission notice appear in all copies.
- *
- *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * Copyright (c) 2016 Open Connectivity Foundation (OCF) and AllJoyn Open
+ *    Source Project (AJOSP) Contributors and others.
+ * 
+ *    SPDX-License-Identifier: Apache-2.0
+ * 
+ *    All rights reserved. This program and the accompanying materials are
+ *    made available under the terms of the Apache License, Version 2.0
+ *    which accompanies this distribution, and is available at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Copyright 2016 Open Connectivity Foundation and Contributors to
+ *    AllSeen Alliance. All rights reserved.
+ * 
+ *    Permission to use, copy, modify, and/or distribute this software for
+ *    any purpose with or without fee is hereby granted, provided that the
+ *    above copyright notice and this permission notice appear in all
+ *    copies.
+ * 
+ *     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *     WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *     WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ *     AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ *     DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ *     PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ *     TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ *     PERFORMANCE OF THIS SOFTWARE.
  ******************************************************************************/
 
 
@@ -110,6 +124,14 @@ class CdmController {
      */
     Ref<CdmInterface> CreateInterface(const std::string& ifaceName, const std::string& busName, const qcc::String& objectPath, const SessionId& sessionId, Ref<InterfaceControllerListener> listener);
 
+    /**
+     * Create interface with the name automatically derived from the name in the <FOO>Interface class.
+     * @param[in] busName bus name
+     * @param[in] objectPath the object path including the interface
+     * @param[in] sessionId session id
+     * @param[in] listener interface listener
+     * @return Interface
+     */
     template <typename T>
     Ref<T> CreateInterface(const std::string& busName, const qcc::String& objectPath, const SessionId& sessionId, Ref<InterfaceControllerListener> listener) {
         return std::static_pointer_cast<T>(CreateInterface(T::INTERFACE_NAME, busName, objectPath, sessionId, listener));
